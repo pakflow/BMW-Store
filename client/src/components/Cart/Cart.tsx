b200c664e4ff4486033ad2015f8d55d6815b91e8
Daniel Savchenko <zzappike@gmail.com>
2023-01-09 18:37:17 +0600

    Add: working cart drawer

---
+++ client/src/components/Cart/Cart.tsx
@@ -0,0 +1,16 @@
+import { FC } from 'react'
+
+const Cart: FC = () => {
+  return (
+    <ul className="menu p-4 w-80 bg-base-100 text-base-content">
+      <li>
+        <a>Sidebar Item 1</a>
+      </li>
+      <li>
+        <a>Sidebar Item 2</a>
+      </li>
+    </ul>
+  )
+}
+
+export default Cart
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,8 @@
 import 'reflect-metadata'
+
+import App from './App'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { container } from 'tsyringe'
-import CartManager from 'utils/cart-manager/CartManager'
-import { cartManagerToken } from 'utils/cart-manager/token'
-import App from './App'
-
-const cartManager = new CartManager()
-container.registerInstance(cartManagerToken, cartManager)
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
